9f65bcd368621f3c7b15c75d8004d28ef0fd005f
Jelena de la Sauce <jelena.sauce@check24.de>
2023-02-19 08:50:50 +0100

    general confirguration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './src/app/components/navigation/navigation.component';
+import { HomeComponent } from './src/app/pages/home/home.component';
+import { ContentCardComponent } from './src/app/components/content-card/content-card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NavigationComponent, HomeComponent, ContentCardComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
