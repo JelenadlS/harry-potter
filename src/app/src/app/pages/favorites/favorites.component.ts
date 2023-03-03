b170143896f3487a8c75746b9a0bbbc7699d7cdf
Jelena de la Sauce <jelena.sauce@check24.de>
2023-03-03 11:18:29 +0100

    Add content to favorite page

--- src/app/src/app/pages/favorites/favorites.component.ts
+++ src/app/src/app/pages/favorites/favorites.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { characters } from '../../services/hp-data.interface';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { filteredList } from '../../store/selector/filter-characters.selector';
 
 @Component({
   selector: 'app-favorites',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favorites.component.less']
 })
 export class FavoritesComponent implements OnInit {
+  public filteredList$:Observable<characters[]> = this.store.select(filteredList);
+  public filteredList: characters[] = [];
 
-  constructor() { }
+  constructor(
+    private store: Store,
+  ) { }
 
   ngOnInit(): void {
+    this.filteredList$
+    .subscribe((characters: characters[]) =>{
+      console.log(characters)
+          this.filteredList = characters
+      }
+    )
   }
-
 }
